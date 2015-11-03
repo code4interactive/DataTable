ff6e0a5cfc7d609d67a4f875c10b22efef4e19b7
Artur Bartczak <artur.bartczak@code4.pl>
2015-11-03 14:44:43 +0100

    L4 Service provider

--- src/DataTableServiceProvider.php
+++ src/DataTableServiceProvider.php
@@ -5,23 +5,24 @@ namespace Code4\DataTable;
 use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
-class DataTableServiceProvider extends ServiceProvider {
+class DataTableServiceProvider4 extends ServiceProvider {
 
     public function register() {
 
         $this->app['datatable'] = $this->app->share(
             function ($app) {
-                $html = $app->make('Collective\Html\HtmlBuilder');
+                $html = $app->make('Illuminate\Html\HtmlBuilder');
                 return new DataTableFactory($html);
             }
         );
+        $this->app['view']->addNamespace('DataTable', __DIR__ . '/../views');
         $this->registerAliases();
     }
 
     public function boot()
     {
-        $this->publishes([__DIR__ . '/../config/datatable.php' => base_path('config/datatable.php')], 'config');
-        $this->loadViewsFrom(__DIR__ . '/../views', 'DataTable');
+        //$this->publishes([__DIR__ . '/../config/datatable.php' => base_path('config/datatable.php')], 'config');
+        //$this->loadViewsFrom(__DIR__ . '/../views', 'DataTable');
     }
 
 
