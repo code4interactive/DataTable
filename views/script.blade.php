b6804f5af7679200502e81506d63f8240e9b804c
Artur Bartczak <artur.bartczak@code4.pl>
2015-11-18 18:45:29 +0100

    Język polski

--- views/script.blade.php
+++ views/script.blade.php
@@ -22,7 +22,31 @@
                     orderable: {!!  $column->isSortable() ? 'true' : 'false' !!}
                 },
                 @endforeach
-                ]
+                ],
+            language: {
+                "sProcessing":   "Przetwarzanie...",
+                "sLengthMenu":   "Pokaż _MENU_ pozycji",
+                "sZeroRecords":  "Nie znaleziono pasujących pozycji",
+                "sInfoThousands":  " ",
+                "sInfo":         "Pozycje od _START_ do _END_ z _TOTAL_ łącznie",
+                "sInfoEmpty":    "Pozycji 0 z 0 dostępnych",
+                "sInfoFiltered": "(filtrowanie spośród _MAX_ dostępnych pozycji)",
+                "sInfoPostFix":  "",
+                "sSearch":       "Szukaj:",
+                "sUrl":          "",
+                "oPaginate": {
+                    "sFirst":    "Pierwsza",
+                    "sPrevious": "Poprzednia",
+                    "sNext":     "Następna",
+                    "sLast":     "Ostatnia"
+                },
+                "sEmptyTable":     "Brak danych",
+                "sLoadingRecords": "Wczytywanie...",
+                "oAria": {
+                    "sSortAscending":  ": aktywuj, by posortować kolumnę rosnąco",
+                    "sSortDescending": ": aktywuj, by posortować kolumnę malejąco"
+                }
+            }
         });
 
         $('#dt-{{$name}}').on( 'processing.dt', function ( e, settings, processing ) {
